26487c740d5f9260474a22a20bd0712736ce994a
3morii74 <omerahmed200237@gmail.com>
2024-07-18 17:30:59 +0300

    create Hero Section

---
+++ portfolio/resources/js/Components/Frontend/Hero.vue
@@ -0,0 +1,70 @@
+<script setup>
+</script>
+<template>
+  <section
+    id="home"
+    class="
+      lg:h-[85vh]
+      flex
+      items-center
+      bg-light-primary
+      dark:bg-dark-primary
+      py-32
+      lg:py-0
+      overflow-hidden
+    "
+  >
+    <div class="container mx-auto h-full">
+      <div class="flex flex-col md:flex-row items-center h-full pt-8">
+        <div class="flex-1 flex flex-col items-center lg:items-start">
+          <p class="text-lg text-accent text-md mb-[22px]">Hey, I'm Omar! 👋</p>
+          <h1
+            class="
+              text-4xl
+              leading-[44px]
+              md:text-5xl md:leading-tight
+              lg:text-7xl lg:leading-[1.2]
+              font-bold
+              md:tracking-[-2px]
+            "
+          >
+            I Build & Design <br />
+            Web Interfaces.
+          </h1>
+          <p
+            class="
+              pt-4
+              pb-8
+              md:pt-6 md:pb-12
+              max-w-[480px]
+              text-lg text-center
+              lg:text-left
+            "
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing illo ad labore
+            dolor elit.
+          </p>
+          <button
+            class="
+              btn btn-md
+              bg-accent
+              hover:bg-light-secondary hover:text-light-tail-500
+              dark:hover:bg-dark-secondary dark:hover:text-dark-navy-100
+              md:btn-lg
+              transition-all
+            "
+          >
+            Work with me
+          </button>
+        </div>
+        <div class="flex flex-1 justify-end items-center h-full mt-8 md:mt-0">
+          <img
+            class="rounded-lg"
+            src="https://scontent.fcai19-7.fna.fbcdn.net/v/t39.30808-6/450559652_2745879958905113_7244495243180391719_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHgFJR655hOT98rfHwiDPjFgoctqUh6E2aChy2pSHoTZtN5x-tpf-8eCkb5u9-J_yWwTwPjmcxMkQkgBqPHaIRZ&_nc_ohc=eizU4A8KQbMQ7kNvgFip-Pa&_nc_ht=scontent.fcai19-7.fna&oh=00_AYD37RuFFIOL__kaxuUX1jNepLkw9uImTICD2cf79DTv-w&oe=669EE586"
+            alt="Hero"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- portfolio/resources/js/Pages/Welcome.vue
+++ portfolio/resources/js/Pages/Welcome.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { Head, Link } from "@inertiajs/inertia-vue3";
 import FrontendLayout from "@/Layouts/Frontend.vue";
+import Hero from "@/Components/Frontend/Hero.vue";
+
 </script>
 
 <template>
   <Head title="Welcome to portfolio" />
   <FrontendLayout>
     <!-- Hero primary -->
+     <Hero/>
     <!-- promote tail-100 -->
     <!-- About secondary -->
     <!-- Skills tail-100 -->
